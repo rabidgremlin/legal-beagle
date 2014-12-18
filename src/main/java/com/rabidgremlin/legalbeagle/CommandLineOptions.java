70f53e37d46dd72e3a236a78f0e398e820a2dd27
rabidgremlin <jack@rabidgremlin.com>
2014-12-18 16:06:06 +1300

    Made command line opts req.

--- src/main/java/com/rabidgremlin/legalbeagle/CommandLineOptions.java
+++ src/main/java/com/rabidgremlin/legalbeagle/CommandLineOptions.java
@@ -4,9 +4,9 @@ import com.beust.jcommander.Parameter;
 
 public class CommandLineOptions
 {
-  @Parameter(names = {"--dir","-d"}, description = "The directory to scan. Will also scan all subdirectories.")
+  @Parameter(names = {"--dir","-d"}, description = "The directory to scan. Will also scan all subdirectories.", required=true)
   public String dir;
   
-  @Parameter(names = {"--output","-o"}, description = "Name of output file. Extension will determine type of output. Currently .tsv or .html are supported.")
+  @Parameter(names = {"--output","-o"}, description = "Name of output file. Extension will determine type of output. Currently .tsv or .html are supported.",required=true)
   public String outputFile;
 }
